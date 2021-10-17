ca51e6b9f7c63be19a143611942a0b7eed0f7f21
Stephan Eicher <stephan.eicher@check24.de>
2021-10-17 13:30:16 +0200

    Drop support for eol software

--- manifests/init.pp
+++ manifests/init.pp
@@ -237,7 +237,7 @@
 #   Defaults to `md5`
 #
 class bamboo (
-  Pattern[/^\d+\.\d+\.\d+(\.\d+)?$/]     $version               = '7.1.4',
+  Pattern[/^\d+\.\d+\.\d+(\.\d+)?$/]     $version               = '8.0.3',
   Enum['tar.gz', 'zip']                  $extension             = 'tar.gz',
   Boolean                                $manage_installdir     = true,
   Stdlib::Unixpath                       $installdir            = '/usr/local/bamboo',
